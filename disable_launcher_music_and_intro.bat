2e38b0a0804ad2eb549273f4baa2e5b181b20def
IIWDTI <60497271+IIWDTI@users.noreply.github.com>
2024-05-08 00:07:37 +0200

    Add files via upload

---
+++ disable_launcher_music_and_intro.bat
@@ -0,0 +1,8 @@
+D:
+cd D:\Steam\steamapps\common\Mass Effect Legendary Edition\Game\Launcher\Content
+rename BWLogo1.bik BWLogo1.bik_
+cd sounds
+rename amb_normandy_looping_quad.wav amb_normandy_looping_quad.wav_
+rename gui_logo_splash_5.1.wav gui_logo_splash_5.1.wav_
+rename mus_gui_credits_quad.wav mus_gui_credits_quad.wav_
+rename mus_gui_menu_looping_quad.wav mus_gui_menu_looping_quad.wav_
---
+++ enable_launcher_music_and_intro.bat
@@ -0,0 +1,8 @@
+D:
+cd D:\Steam\steamapps\common\Mass Effect Legendary Edition\Game\Launcher\Content
+rename BWLogo1.bik_ BWLogo1.bik
+cd sounds
+rename amb_normandy_looping_quad.wav_ amb_normandy_looping_quad.wav
+rename gui_logo_splash_5.1.wav_ gui_logo_splash_5.1.wav
+rename mus_gui_credits_quad.wav_ mus_gui_credits_quad.wav
+rename mus_gui_menu_looping_quad.wav_ mus_gui_menu_looping_quad.wav
